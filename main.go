72a3a4c02dd4b3c3932223040596e4437e72f339
James Tran <tmnhat2001@gmail.com>
2020-07-06 21:59:00 -0400

    Initial commit

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+  "github.com/bwmarrin/discordgo"
+  "fmt"
+  "os"
+  "os/signal"
+  "syscall"
+)
+
+func main() {
+  fmt.Println("Testing Discord bot")
+
+  token := os.Getenv("BOT_TOKEN")
+  discord, err := discordgo.New("Bot " + token)
+  if err != nil {
+    fmt.Println("Error creating Discord session: ", err)
+    return
+  }
+
+  discord.AddHandler(messageCreate)
+
+  err = discord.Open()
+  if err != nil {
+    fmt.Println("Error opening connection: ", err)
+    return
+  }
+
+  fmt.Println("Bot is now running. Press CTRL-C to exit.")
+  sc := make(chan os.Signal, 1)
+  signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+  <-sc
+
+  discord.Close()
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+  if m.Author.ID == s.State.User.ID {
+    return
+  }
+
+  if m.Content == "haikyuu!" {
+    s.ChannelMessageSend(m.ChannelID, "Go Karasuno!")
+  }
+
+  if m.Content == "sasuke!" {
+    s.ChannelMessageSend(m.ChannelID, "naruto!")
+  }
+}
